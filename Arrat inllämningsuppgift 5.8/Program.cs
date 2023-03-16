0078c2b35c6fb6da6f9e9fb78a689a75f8c474f8
Pingvu <nhlpool27@gmail.com>
2023-03-16 13:58:12 +0100

    Add project files.

---
+++ Arrat inllämningsuppgift 5.8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+namespace Array
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] Tal = new int[5];
+            Tal[0] = 148;
+            Tal[1] = 669;
+            Tal[2] = -137;
+            Tal[3] = 734;
+            Tal[4] = 999;
+            
+            string[] d = new string[10000];
+
+            for (int i = 0; true; i++)
+            {
+                
+                string c = Tal[i].ToString();
+                 d[i] = c;
+                
+                Console.Write(d[i]);
+                Thread.Sleep(5000);
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                
+              
+            }
+        }
+    }
+}
